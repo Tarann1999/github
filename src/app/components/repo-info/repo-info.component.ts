3b9f4dfbc03598710ae0255af1d4983654b9ce26
Tarann1999 <taranjeet.birdi@iauro.com>
2024-08-28 15:20:53 +0530

    Repository Changes

--- src/app/components/repo-info/repo-info.component.ts
+++ src/app/components/repo-info/repo-info.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { SvgRepoComponent } from '../svg-repo/svg-repo.component';
 import { Store } from '@ngrx/store'; 
 import { Observable } from 'rxjs/internal/Observable';
+import { lastValueFrom } from 'rxjs/internal/lastValueFrom';
 
 @Component({
   selector: 'app-repo-info',
@@ -21,23 +22,23 @@ export class RepoInfoComponent implements OnInit {
   constructor(private provider:ProviderService,private store:Store<{repoInformation:any[]}>){
     this.storeRepoInformation$ = store.select("repoInformation")
   } 
-  ngOnInit(): void {
+  ngOnInit() {
     // if(this.information!=undefined){
-      this.storeRepoInformation$.subscribe((res:any)=>{
+      this.storeRepoInformation$.subscribe(async (res:any)=>{
         console.log(res)
         this.information=res[res.length-1];
         // this.information = this.provider.SelectedUserInfo;
-        this.provider.getAUser(this.information.owner.login).subscribe(async (user:any)=>{
-          this.information=user[user.length-1];
+        var userInformation = await lastValueFrom(this.provider.getAUser(this.information.owner.login))
+          // this.information=userInformation;
           console.log("this.information",this.information);
-          this.provider.fetchData(this.information.contents_url.slice(0,(this.information.contents_url.indexOf('{'))-1)).subscribe((data:any)=>{
+          const data = await  lastValueFrom(this.provider.fetchData(this.information.contents_url.slice(0,(this.information.contents_url.indexOf('{'))-1)))
             this.foldersList = data
-          })
+          // })
           // this.profilepath = res.avatar_url
           //  this.repoList = await lastValueFrom(this.provider.fetchData(res.repos_url))
           //  console.log(this.repoList)
           })
-      })
+      // })
       // this.information = this.provider.SelectedRepoInfo
       // this.provider.fetchData(this.information.contents_url.slice(0,(this.information.contents_url.indexOf('{'))-1)).subscribe((res:any)=>{
       //   this.foldersList = res
